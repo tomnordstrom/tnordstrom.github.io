4d2362e0ecf0422b871defd85dac2fd601da7b3a
TamiAlQuida <whozie@gmail.com>
2024-10-25 14:07:31 +0200

    Clean up

--- Jobb/Obsolete_spare_parts/format_csv.py
+++ Jobb/Obsolete_spare_parts/format_csv.py
@@ -1,46 +1,54 @@
 import csv
 import pandas as pd
+import os
 
-df = pd.read_csv('data\c20.csv')
+# Get the directory of the current script
+current_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Remove the last 4 characters from the 'File Name' column
-df['File Name'].str[:-4].to_csv('data\c20_formatted.csv', mode= 'w', index=False)
+machines = ['c20', 'c23']
 
-df = pd.read_csv('data\c20_formatted.csv')
+def format_csv():
+    for i in range(len(machines)):
+        machine = machines[i]
+        # Use os.path.join to create proper file paths
+        input_file = os.path.join(current_dir, 'data/input', f'{machine}.csv')
+        df = pd.read_csv(input_file)
 
-# Find duplicates based on 'File Name' column
-duplicates = df[df.duplicated(subset=['File Name'], keep=False)]
+        # Remove the last 4 characters from the 'File Name' column
+        formatted_file = os.path.join(current_dir, 'data/output', f'{machine}_formatted.csv')
+        df['File Name'].str[:-4].to_csv(formatted_file, mode='w', index=False)
 
-# Save duplicates to a new CSV file
-duplicates.to_csv('data\c20_duplicates.csv', index=False)
+        df = pd.read_csv(formatted_file)
 
-# Mark duplicates in the original DataFrame (except for first duplicate)
-df['Duplicate'] = df.duplicated(subset=['File Name'], keep='first')
+        # Find duplicates based on 'File Name' column
+        duplicates = df[df.duplicated(subset=['File Name'], keep='first')]
 
-# Save the updated DataFrame with the 'Duplicate' column
-df.to_csv('data\c20_with_duplicates.csv', index=False)
+        # Save duplicates to a new CSV file
+        duplicates_file = os.path.join(current_dir, 'data/output', f'{machine}_duplicates.csv')
+        duplicates.to_csv(duplicates_file, index=False)
 
-# Create a new DataFrame for non-duplicate entries
-non_duplicates = df[~df['Duplicate']]
+        # Mark duplicates in the original DataFrame (except for first duplicate)
+        df['Duplicate'] = df.duplicated(subset=['File Name'], keep='first')
 
-# Save all non-duplicate entries to the CSV file
-non_duplicates['File Name'].to_csv('data\c20_without_duplicates.csv', index=False)
+        # Save the updated DataFrame with the 'Duplicate' column
+        df.to_csv(os.path.join(current_dir, 'data/output', f'{machine}_with_duplicates.csv'), index=False)
 
-for i in range(len(non_duplicates)):
-    if i < 200:
-        with open('data\c20.txt', 'a') as file:
-            file.write(str(non_duplicates.iloc[i]['File Name']) + ' or ')  
-    elif i == 200:
-        with open('data\c20.txt', 'a') as file:
-            file.write("\n" + str(non_duplicates.iloc[i]['File Name']) + ' or ')  
-    else:
-        with open('data\c20.txt', 'a') as file:
-            file.write(str(non_duplicates.iloc[i]['File Name']) + ' or ')  
+        # Create a new DataFrame for non-duplicate entries
+        non_duplicates = df[~df['Duplicate']]
 
-#    if df.iloc[i]['Duplicate'] == False:
-#        df.iloc[i]['File Name'].to_csv('data\c20_without_duplicates.csv', mode= 'w', index=False)
-#    print(df.iloc[i]['File Name'][:-4])
-#    print(' or ')
-#    with open('data\c20.txt', 'a') as file:
-#        
-#        file.write(df.iloc[i]['File Name'][:-4] + ' or ')  
+        # Save all non-duplicate entries to the CSV file
+        non_duplicates['File Name'].to_csv(os.path.join(current_dir, 'data/output', f'{machine}_without_duplicates.csv'), index=False)
+
+        output_txt = os.path.join(current_dir, 'data/output', f'{machine}.txt')
+        for i in range(len(non_duplicates)):
+            with open(output_txt, 'a') as file:
+                if i < 200:
+                    file.write(str(non_duplicates.iloc[i]['File Name']) + ' or ')
+                elif i == 200:
+                    file.write("\n" + str(non_duplicates.iloc[i]['File Name']) + ' or ')
+                else:
+                    file.write(str(non_duplicates.iloc[i]['File Name']) + ' or ')
+
+
+if __name__ == "__main__":
+    format_csv()
